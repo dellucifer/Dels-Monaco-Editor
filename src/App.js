19291e38db03270b898f4bac87df565a0c42d6b5
Dellucifer <pg99285@gmail.com>
2023-07-01 15:32:21 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Editor } from "@monaco-editor/react";
 
 function App() {
+  const options = {
+    fontSize: 18,
+  };
+
+  const c = {
+    language: 'c',
+    template: `#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+    
+    return 0;
+}`
+  }
+
+  const cpp = {
+    language: 'cpp',
+    template: `#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}`
+  }
+
+  const java = {
+    language: 'java',
+    template: `public class Main
+{
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}`
+  }
+
+  const python = {
+    language: 'python',
+    template: `# Enter you code here`
+  }
+
+  const [language, setLanguage] = useState(c);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+        <button onClick={() => setLanguage(c)}>C</button>
+        <button onClick={() => setLanguage(cpp)}>C++</button>
+        <button onClick={() => setLanguage(java)}>Java</button>
+        <button onClick={() => setLanguage(python)}>Python</button>
+      </div>
+
+      <div className="editor">
+        <Editor
+          // height="100vh"
+          minimap="true"
+          language={language.language}
+          options={options}
+          height="calc(100vh - 50px)"
+          width="100%"
+          theme="vs-dark"
+          value={language.template}
+        />
+      </div>
+      <div style={{zIndex: 10, position: "absolute", bottom: 0, marginLeft: "10px"}}>Powered by Dellucifer</div>
     </div>
   );
 }
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <button>C</button>
+      <button>C++</button>
+      <button>Java</button>
+      <button>Python</button>
+    </div>
+  )
+}
+
+export default Navbar
